e12ed5e32de6b7f6c7a5f7468f0a2ea97216bfac
Arturo Aguila <agu09001@byui.edu>
2017-11-02 15:49:27 -0600

    pseudocode

--- game.cpp
+++ game.cpp
@@ -3,4 +3,18 @@
 using namespace std;
 int main(int argc, char *argv[]) {
 	
+	// parent process - Steve
+	
+	// fork child process - Jim
+	
+	
+	// both Steve and Jim throw a rock, paper, or scissors
+	
+	
+	// Steve gets Jims throw and decides the winner
+	
+	
+	
+	
+	
 }
