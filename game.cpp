1c39fd8d1dfdbad16d10acd75230738092c5c760
William Willden <wwillden@outlook.com>
2017-11-02 16:12:28 -0600

    Added more Pseudocode

--- game.cpp
+++ game.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 
+enum Selection {ROCK, PAPER, SCISSORS};
+
+void sig_handler(int signo) {
+    if (signo == SIGUSR1) {
+        //Compare ints
+        std::cout << "Signal Handled\n";
+    }
+}
+
 using namespace std;
 int main(int argc, char *argv[]) {
+
+    pid_t steve;
+    pid_t jimmy;
 	
 	// parent process - Steve
 	
 	// fork child process - Jim
-	
+	if (! (pid_t temp = fork()) {
+        jimmy = temp;
+        //child process
+    }
+    else {
+        //parent process
+    }
 	
 	// both Steve and Jim throw a rock, paper, or scissors
 	
